fc216cede284fa208b89a078fdf9ba7f2eb3b435
Hardy143 <vickihardy88@gmail.com>
2018-07-31 15:33:05 +0100

    Simple phone tilt app

--- AngleDetector/ViewController.swift
+++ AngleDetector/ViewController.swift
@@ -7,12 +7,69 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var upArrow: UIImageView!
+    @IBOutlet weak var downArrow: UIImageView!
+    
+    let motionManger = CMMotionManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        upArrow.isHidden = true
+        downArrow.isHidden = true
+        detectMotion()
+    }
+    
+    // Set up motion detector
+    func detectMotion() {
+        
+        if motionManger.isDeviceMotionAvailable {
+            // do something
+            motionManger.startDeviceMotionUpdates(
+                to: OperationQueue.current!, withHandler: {
+                    (deviceMotion, error) -> Void in
+                    
+                    if(error == nil) {
+                        self.motionManger.deviceMotionUpdateInterval = 0.1
+                        self.displayArrows(deviceAngle: self.handleDeviceMotionUpdate(deviceMotion: deviceMotion!)) 
+                    } else {
+                        //handle the error
+                    }
+            })
+        }
+    }
+    
+    // Convert radians to degrees
+    func radiansToDegrees(_ radians: Double) -> Double {
+        return radians * (180.0 / Double.pi)
+    }
+    
+    // Print out the degrees of the device tilt
+    func handleDeviceMotionUpdate(deviceMotion: CMDeviceMotion) -> CGFloat {
+        let attitude = deviceMotion.attitude
+        let quat = attitude.quaternion
+        let qPitch = CGFloat(radiansToDegrees(atan2(2 * (quat.x * quat.w + quat.y * quat.z), 1 - 2 * quat.x * quat.x - 2 * quat.z * quat.z)))
+        let numOfDegrees = String(format: "%.0f", qPitch)
+        print("Degrees: \(numOfDegrees)")
+        return qPitch
+    }
+    
+    // show arrows
+    func displayArrows(deviceAngle: CGFloat) {
+        if deviceAngle > 105 {
+            downArrow.isHidden = false
+            upArrow.isHidden = true
+        } else if  deviceAngle < 80 {
+            upArrow.isHidden = false
+            downArrow.isHidden = true
+        } else {
+            downArrow.isHidden = true
+            upArrow.isHidden = true
+        }
     }
 
     override func didReceiveMemoryWarning() {
